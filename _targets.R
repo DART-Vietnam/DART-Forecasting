ea83a97a45b01f982e2026e8383341205360b5c9
tuyenhn <tuyenhn@oucru.org>
2026-03-26 11:51:04 +0000

    refactor targets setup and better crew logging

--- _targets.R
+++ _targets.R
@@ -1,59 +1,7 @@
-library(autometric)
-library(crew)
-library(targets)
-suppressPackageStartupMessages(library(tidyverse))
+# Edit the pipeline setup in `targets_setup.R`
+source("targets_setup.R")
 
-# Load mlr3 stuff and ML learners here
-# so that qs2 deserialisaion can happen correctly
-# when loading mlr3 learners from qs2 files
-library(mlr3)
-library(mlr3pipelines)
-library(mlr3learners)
-library(ranger)
-
-# load envs (from docker compose file)
-## crew workers for branch parallelisation
-.env_crew_workers <- as.numeric(Sys.getenv("CREW_WORKERS"))
-.crew_workers <- if (is.na(.env_crew_workers)) {
-  4L
-} else {
-  .env_crew_workers
-}
-
-## load TOML container run-time configs
-.toml_fpath <- Sys.getenv("TOML_CONF_FPATH")
-if (.toml_fpath == "") {
-  stop("`TOML_CONF_FPATH` env var must be supplied")
-}
-
-log_dir <- "_targets/logs/"
-
-# setup crew workers
-.local_crew_controller <- crew_controller_local(
-  workers = .crew_workers,
-  options_metrics = crew_options_metrics(
-    path = paste0(log_dir, "local_crew_workers"),
-    seconds_interval = 1
-  )
-)
-
-# setup targets options
-tar_source("target_fns")
-tar_option_set(
-  packages = c("tidyverse", "data.table"),
-  format = "qs",
-  seed = 764,
-  controller = .local_crew_controller
-)
-
-# setup `autometric` logging for trunk process
-if (tar_active()) {
-  log_start(
-    path = paste0(log_dir, "target_trunk.txt"),
-    seconds = 1
-  )
-}
-
+# Here, it's just the pipeline
 list(
   #
   # Load run-time config
