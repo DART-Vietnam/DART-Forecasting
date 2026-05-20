c64fdffc1962bd877e236793b016ac966f2eba77
Tuyen Huynh <hntuyen282@gmail.com>
2026-05-20 13:05:01 +0700

    update container watch script

--- container_watcher.R
+++ container_watcher.R
@@ -1,8 +1,9 @@
 library(targets)
 
+Sys.setenv(TOML_CONF_FPATH = "targets_config.toml")
+
 tar_config_set(
-  store = "container_targets",
-  project = "dart-forecasting-container"
+  store = "container_targets"
 )
 tar_path_store()
 
